7f8838b88a3763497390f8861e55689a2609a450
José Antonio <jtony.net@gmail.com>
2017-05-22 20:02:50 -0300

    intercpeting proxy methods

---
+++ client/js/app/services/ProxyFactory.js
@@ -0,0 +1,31 @@
+class ProxyFactory {
+
+    static create(objeto, props, acao) {
+
+        return new Proxy(new ListaNegociacoes(), {
+            get(target, prop, receiver) {
+                if (props.includes(prop) && typeof(target[prop]) == typeof(Function)) {
+
+                    return function() {
+                        console.log(`interceptando ${prop}`);
+                        Reflect.apply(target[prop], target, arguments);
+                        return acao(target);
+                    }
+                }
+
+                return Reflect.get(target, prop, receiver);
+            },
+
+            set(target, prop, value, receiver) {
+                if (props.includes(prop)) {
+                    //target[prop] = value;
+                    acao(target);
+                }
+
+                return Reflect.set(target, prop, value, receiver);
+            }
+
+
+        });
+    }
+}
